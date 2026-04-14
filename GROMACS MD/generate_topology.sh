95576b6c53031e61f0f6998f0b87d97a27055a3d
yasnanayabkhi <yasna_nayabkhil@berkeley.edu>
2026-04-13 21:07:42 -0700

    fixed generate topology sh

--- GROMACS MD/generate_topology.sh
+++ GROMACS MD/generate_topology.sh
@@ -10,15 +10,13 @@
 # Generates 3 files: topology file, position restraint file, post-procssed structure file
 grep -v HOH FtsZ-Dacomitinib128.pdb > FtsZ-Dacomitinib_clean.pdb
 
+# To fix ASN 25 residue (before doing pdb2gmx) 
+pdbfixer FtsZ-Dacomitinib_clean.pdb --output=FtsZ-Dacomitinib_fixed.pdb --add-atoms=all --add-residues
+
 # Processed with tip3p water
 # Generates 1 file: processed
 # echo 1 to choose 1st choice for force field (charmm36)
-echo 1 | gmx pdb2gmx -f FtsZ-Dacomitinib_clean.pdb -o FtsZ-Dacomitinib_processed.gro -water tip3p
-
-# To fix ASN 25 residue 
-gmx pdb2gmx -f FtsZ-Dacomitinib_fixed.pdb -o FtsZ-Dacomitinib_processed.gro -water tip3p
-
-#
+echo 1 | gmx pdb2gmx -f FtsZ-Dacomitinib_fixed3.pdb -o FtsZ-Dacomitinib_processed.gro -water tip3p
 
 # tell user generating topology is done 
 echo "Topology generation is completed.\n"
